437632d4e53bda181088a6ea9d4ab59e9f74c56a
James Sandridge <jims@radical-ideal.com>
2016-06-17 21:07:00 -0400

    init

---
+++ one/README.md
@@ -0,0 +1,11 @@
+## Easy as...
+
+1. Install Stack (http://docs.haskellstack.org/en/stable/README/)
+2. ```stack setup && stack build```
+3. ```stack exec one```
+
+
+```
+Sum is: 234168
+86.99 µs
+```
---
+++ one/app/Main.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Exception
+import Formatting
+import Formatting.Clock
+import System.Clock
+
+main =
+    do
+      start <- getTime Monotonic
+      a <- evaluate(sum [i | i <- [1..1000], i `mod` 3 == 0 || i `mod` 5 == 0])
+      end <- getTime Monotonic
+
+      fprint ("Sum is: " % int % "\n") a
+      fprint (timeSpecs % "\n") start end
+
